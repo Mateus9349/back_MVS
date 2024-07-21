d44b742bd5ab1c144382febed4ec884c9337a845
unknown <mateusvpdl@outlook.com>
2024-07-21 15:58:17 -0300

    atualizando os informações

--- api/index.js
+++ api/index.js
@@ -1,12 +1,23 @@
+const fs = require('fs');
+const https = require('https');
+
 const express = require('express');
 const routes = require('./routes')
 
 const app = express();
 
-const port = 9000;
+const port = 9001;
 
 routes(app)
 
-app.listen(port, ()=> console.log(`O servidor esta rodando na porta ${port}`))
+app.listen(port, ()=> console.log(`O servidor esta rodando na porta ${port}`));
+
+https.createServer({
+    cert: fs.readFileSync('api/SSL/certificate.crt'),
+    key: fs.readFileSync('api/SSL/private.key'),
+    ca: fs.readFileSync('api/SSL/ca_bundle.crt')
+}, app).listen(4001, ()=> console.log('Rodando em https'));
+
+
 
 module.exports = app

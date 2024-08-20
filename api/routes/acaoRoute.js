465d95a237652b0e578c7ff9ce780978e5a3ede1
unknown <mateusvpdl@outlook.com>
2024-08-20 10:52:14 -0300

    adicionando o certificado SSL

--- api/routes/acaoRoute.js
+++ api/routes/acaoRoute.js
@@ -6,7 +6,7 @@ const acaoController = new AcaoController();
 const router = Router()
 
 router.get('/acao', (req, res) => acaoController.pegaTodos(req, res));
-router.get('/acao/:id', (req,res) => acaoController.pegaUmPorId(req,res)) 
+router.get('/acao/:id', (req,res) => acaoController.pegaUmPorId(req,res)); 
 router.post('/acao', (req, res) => acaoController.criaNovo(req, res));
 router.put('/acao/:id', (req, res) => acaoController.atualiza(req,res));
 router.delete('/acao/:id', (req, res) => acaoController.exclui(req, res));
